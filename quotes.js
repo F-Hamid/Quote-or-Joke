a69798a378a0f0e8dae9904d3a4c3877676f553e
F-Hamid <fateh_hamid81@hotmail.com>
2025-01-07 09:19:33 +0100

    quote API fix - replace with go-quote

--- quotes.js
+++ quotes.js
@@ -11,16 +11,17 @@ const loader = document.getElementById("loader");
 
 async function getQuote() {
   loading();
-  const apiUrl = "https://type.fit/api/quotes";
+  const apiUrl = "https://go-quote.azurewebsites.net/random-quote?format=json"
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    const randomQuote = Math.floor(Math.random() * data.length);
-    const quote = data[randomQuote];
 
-    // injection quote
+    // const randomQuote = Math.floor(Math.random() * data.length);
+    // const quote = data[randomQuote];
+    // console.log(quote)
 
-    if (quote.text.length > 120) {
+
+    if (data.text.length > 120) {
       quoteText.classList.add("long-quote");
     } else {
       quoteText.classList.remove("long-quote");
@@ -28,15 +29,15 @@ async function getQuote() {
     if (quote.author === null) {
       quote.author = "Unknown";
     } else {
-      quoteAuthor.innerText = quote.author;
+      quoteAuthor.innerText = data.author;
     }
-    quoteText.innerText = quote.text;
+    quoteText.innerText = data.text;
 
     // Stop loader
     complete();
   } catch (e) {
     console.log("Ooops somthing goes wrong !!, try again", e);
-    getQuote();
+    // getQuote();
   }
   // document.querySelector("#new-quote").addEventListener("click", getQuote);
 }
